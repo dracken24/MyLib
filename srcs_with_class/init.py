73e33825e11b10b9a168383b876c9cceb20c4345
dracken24 <dracken24@gmail.com>
2024-12-31 12:07:07 -0500

    Work on split and defile text in box view

--- srcs_with_class/init.py
+++ srcs_with_class/init.py
@@ -114,12 +114,14 @@ quitter: MyButton = MyButton(10,
 							 (BUTTON_HEIGHT * 10 + 25),
 							 (WINDOW_WIDTH - 20),
 							 (BUTTON_HEIGHT - 5),
-							 BLUE,
-							 DARKGREEN,
-							 DARKBLUE,
+							 ORANGE,
+							 RED,
+							 MAROON,
 							 WHITE,
                              "Quitter",
 							 20)
+
+lorem: str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 # init principal variables for the program
 def init():
--- srcs_with_class/main.py
+++ srcs_with_class/main.py
@@ -3,13 +3,27 @@ from init import *
 from buttons_functions import *
 from my_button import *
 
+def adjust_text_in_box_and_draw_result(box: Rectangle, text: str, line_position: int = 0):
+    text_width = measure_text(text.encode('utf-8'), 20)
+    line_height = 22
+    writable_length = int(box.width - 20)
+
+    if text_width > writable_length:
+        line = text[:int(writable_length / 10.2)]
+        text = text[int(writable_length / 10.2):]
+
+        adjust_text_in_box_and_draw_result(box, text, line_height + line_position)
+        draw_text(line, int(box.x + 10), int(box.y + line_position), 20, BLACK)
+    else:
+        draw_text(text, int(box.x + 10), int(box.y + line_position), 20, BLACK)
+
 def main():
 
     init()
 
     # For Quit the main loop for exit programm
     quit_ct: bool = False
-    affich_text: str = ""
+    affich_text: str = lorem
 
     while (not quit_ct and not window_should_close()):    # Detect window close on x corner click or escape key
         # Drawing begin
@@ -27,17 +41,17 @@ def main():
             button = dict_button[button_key]
             if isinstance(button, MyButton):
                 text = button.draw_button()
-                if text is not None:
+                if text is not None and text != "Quitter":
                     affich_text = text
+                if (text == "Quitter"):
+                    quit_ct = True
         
         # Draw text zone
         draw_rectangle(10, int(WINDOW_HEIGHT / 4 * 3), int(WINDOW_WIDTH - 20), int(WINDOW_HEIGHT / 4 - 10), WHITE)
-        draw_text(affich_text, 20, int(WINDOW_HEIGHT / 4 * 3) + 10, 20, BLACK)
+        # draw_text(affich_text, 20, int(WINDOW_HEIGHT / 4 * 3) + 10, 20, BLACK)
+        adjust_text_in_box_and_draw_result(Rectangle(10, int(WINDOW_HEIGHT / 4 * 3), int(WINDOW_WIDTH - 20), int(WINDOW_HEIGHT / 4 - 10)), affich_text)
 
         draw_text(WINDOW_TITLE.encode('utf-8'), x_position, 15, 20, DARKGRAY)
-
-        if (affich_text == "Quitter"):
-            quit_ct = True
 
         end_drawing()
 
